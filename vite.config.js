4ee5278fcfe30e6412cb34de4b834d585a9cd5de
Richard West <r.west@northeastern.edu>
2025-12-28 23:05:45 -0800

    Add GitHub Actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/scales-tuner/',
   server: {
     allowedHosts: ['.ngrok-free.dev', '.ngrok.io']
   }
